3662057d0e419f0f7e5fb942aa9070f0064b5b09
lxy <lgjd33402@gmail.com>
2017-05-05 17:05:38 +0800

    Initial Commit

---
+++ UZChess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UZChess
+//
+//  Created by Xiaoyu Liu on 17/5/5.
+//  Copyright © 2017年 com.uzero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UZChess/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UZChess
+//
+//  Created by Xiaoyu Liu on 17/5/5.
+//  Copyright © 2017年 com.uzero. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
